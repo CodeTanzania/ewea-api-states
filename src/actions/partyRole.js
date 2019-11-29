c017dc5ae6c6c67ba8aafcbe7cb7c2813ac0371f
Benson Maruchu <benmaruchu@gmail.com>
2019-11-29 15:45:01 +0300

    feat: add party role on store and refactor code a bit

---
+++ src/actions/partyRole.js
@@ -0,0 +1,23 @@
+import generateExposedActions from '../factories/action';
+import { actions, dispatch } from '../store';
+
+const partyRoleActions = generateExposedActions('partyRole', actions, dispatch);
+
+export const {
+  clearPartyRoleFilters,
+  clearPartyRolesSort,
+  closePartyRoleForm,
+  deletePartyRole,
+  filterPartyRoles,
+  getPartyRoles,
+  getPartyRole,
+  selectPartyRole,
+  openPartyRoleForm,
+  paginatePartyRoles,
+  postPartyRole,
+  putPartyRole,
+  refreshPartyRoles,
+  searchPartyRoles,
+  setPartyRoleSchema,
+  sortPartyRoles,
+} = partyRoleActions;
