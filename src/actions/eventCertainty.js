dba197abd3ce719867ced26b5341c4aa6ff15b2e
Benson Maruchu <benmaruchu@gmail.com>
2019-12-17 23:47:15 +0300

    feat(store): add event question and event certainty

---
+++ src/actions/eventCertainty.js
@@ -0,0 +1,27 @@
+import generateExposedActions from '../factories/action';
+import { actions, dispatch } from '../store';
+
+const eventCertaintyActions = generateExposedActions(
+  'eventCertainty',
+  actions,
+  dispatch
+);
+
+export const {
+  clearEventCertaintyFilters,
+  clearEventCertaintiesSort,
+  closeEventCertaintyForm,
+  deleteEventCertainty,
+  filterEventCertainties,
+  getEventCertainties,
+  getEventCertainty,
+  selectEventCertainty,
+  openEventCertaintyForm,
+  paginateEventCertainties,
+  postEventCertainty,
+  putEventCertainty,
+  refreshEventCertainties,
+  searchEventCertainties,
+  setEventCertaintySchema,
+  sortEventCertainties,
+} = eventCertaintyActions;
