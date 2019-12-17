c3c2db0479e88bf46c80cbabb2db7b031f4cdc9a
Benson Maruchu <benmaruchu@gmail.com>
2019-12-17 23:28:39 +0300

    feat(store): add event severity resource

---
+++ src/actions/eventSeverity.js
@@ -0,0 +1,27 @@
+import generateExposedActions from '../factories/action';
+import { actions, dispatch } from '../store';
+
+const eventSeverityActions = generateExposedActions(
+  'eventSeverity',
+  actions,
+  dispatch
+);
+
+export const {
+  clearEventSeverityFilters,
+  clearEventSeveritiesSort,
+  closeEventSeverityForm,
+  deleteEventSeverity,
+  filterEventSeverities,
+  getEventSeverities,
+  getEventSeverity,
+  selectEventSeverity,
+  openEventSeverityForm,
+  paginateEventSeverities,
+  postEventSeverity,
+  putEventSeverity,
+  refreshEventSeverities,
+  searchEventSeverities,
+  setEventSeveritySchema,
+  sortEventSeverities,
+} = eventSeverityActions;
